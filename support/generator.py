3df5eca1bac88afebb22afa90ec3d44821cc48cd
João Marques <joaomosm@gmail.com>
2019-11-02 10:42:39 +0000

    generator: refactor code

--- support/generator.py
+++ support/generator.py
@@ -1,58 +1,74 @@
 import sys
 import os
 
-args_len = len(sys.argv)
 
-if len(sys.argv) != 3:
-  print 'Wrong number of arguments (', args_len - 1, 'intead of 2).'
-# elif:
-  # TODO: safely cast arguments
+# --- Function Definitions ---
+def read_arguments():
+  args_len = len(sys.argv)
 
-puzzle_year = int(sys.argv[1])
-puzzle_day = int(sys.argv[2])
+  if len(sys.argv) != 3:
+    print 'Wrong number of arguments:', args_len - 1, 'intead of 2.'
+    exit()
+  # elif:
+    # TODO: safely cast arguments
 
-print 'year number ->', puzzle_year,'and puzzle day ->', puzzle_day
+  puzzle_year = int(sys.argv[1])
+  puzzle_day = int(sys.argv[2])
+  print 'year number ->', puzzle_year,'and puzzle day ->', puzzle_day
 
-if puzzle_year < 2015 or puzzle_year > 2019:
-  print 'Puzzle year must be between 2015 and 2019.'
-elif puzzle_day < 1 or puzzle_day > 25:
-  print 'Puzzle day must be between 1 and 25.'
-# elif
-  # TODO: prevent generating files already created
+  return puzzle_year, puzzle_day
 
-path = '{year}/{day}/'.format(year = puzzle_year, day = puzzle_day)
-filenames = [
-  '{day}_answers.txt'.format(day = puzzle_day),
-  '{day}_part_one_input.txt'.format(day = puzzle_day),
-  '{day}_part_one.py'.format(day = puzzle_day),
-  '{day}_part_two_input.txt'.format(day = puzzle_day),
-  '{day}_part_two.py'.format(day = puzzle_day),
-]
+def validate_arguments(puzzle_year, puzzle_day):
+  if puzzle_year < 2015 or puzzle_year > 2019:
+    print 'Puzzle year must be between 2015 and 2019.'
+    exit()
+  elif puzzle_day < 1 or puzzle_day > 25:
+    print 'Puzzle day must be between 1 and 25.'
+    exit()
+  # elif
+    # TODO: prevent generating files already created
 
-try:
+def generate_path_and_filenames(puzzle_year, puzzle_day):
+  path = '{year}/{day}/'.format(year = puzzle_year, day = puzzle_day)
+  filenames = [
+    '{day}_answers.txt'.format(day = puzzle_day),
+    '{day}_part_one_input.txt'.format(day = puzzle_day),
+    '{day}_part_one.py'.format(day = puzzle_day),
+    '{day}_part_two_input.txt'.format(day = puzzle_day),
+    '{day}_part_two.py'.format(day = puzzle_day),
+  ]
+  try:
     os.mkdir(path)
-except OSError:
-    print ("Creation of the directory %s failed" % path)
-else:
-    print ("Successfully created the directory %s " % path)
+  except OSError:
+      print ("Creation of the directory %s failed" % path)
+      exit()
+  else:
+      print ("Successfully created the directory %s " % path)
+
+  return path, filenames
+
+def create_file(path_and_filename, content):
+  f = open(path_and_filename, 'w+')
+  f.write(content)
+  f.close
+
+
+# --- Script ---
+puzzle_year, puzzle_day = read_arguments()
+validate_arguments(puzzle_year, puzzle_day)
+path, filenames = generate_path_and_filenames(puzzle_year, puzzle_day)
 
 # Create answers file
-f0 = open(path + filenames[0],"w+")
-f0.write('part_one = <answer_part_one>\npart_two = <answer_part_two>')
-f0.close
+create_file(path + filenames[0], 'part_one = <answer_part_one>\npart_two = <answer_part_two>')
 
 # Create part one input file
-f1 = open(path + filenames[1],"w+")
-f1.close
+create_file(path + filenames[1], '')
 
 # Create part one python file
-f2 = open(path + filenames[2],"w+")
-f2.close
+create_file(path + filenames[2], '')
 
 # Create part two input file
-f3 = open(path + filenames[3],"w+")
-f3.close
+create_file(path + filenames[3], '')
 
 # Create part two python file
-f4 = open(path + filenames[4],"w+")
-f4.close
+create_file(path + filenames[4], '')
