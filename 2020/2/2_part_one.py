bbb966f6ded7d70c9f79edcbf24a51cc93f82298
João Marques <joaomosm@gmail.com>
2020-12-04 01:27:17 +0000

    2020.2: complete part one

---
+++ 2020/2/2_part_one.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def parse_input(content):
+  list = []
+  for c in content:
+    temp1 = c.split("-")
+    temp2 = temp1[1].split(" ")
+    temp3 = temp2[1].split(":")
+
+    min_times = temp1[0]
+    max_times = temp2[0]
+    char_to_check = temp3[0]
+    password_to_check = temp2[2]
+
+    hash = {
+      'min': int(temp1[0]),
+      'max': int(temp2[0]),
+      'char': temp3[0],
+      'password' :temp2[2]
+    }
+    list.append(hash)
+  return list
+
+def solve_puzzle(input_list):
+  sum = 0
+  for k in input_list:
+    count = k['password'].count(k['char'])
+    if(count >= k['min'] and count <= k['max']):
+      sum += 1
+
+
+  return sum
+
+with open('2_part_one_input.txt') as f:
+  content = f.read().splitlines()
+
+input_list = parse_input(content)
+result = solve_puzzle(input_list)
+
+print(result)
