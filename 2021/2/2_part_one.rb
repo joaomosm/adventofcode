b82facc97a5bae286577f78457a0942f96adef35
João Marques <joaom@mail.com>
2021-12-02 20:47:30 +0000

    2021.2: complete part one

---
+++ 2021/2/2_part_one.rb
@@ -0,0 +1,16 @@
+require './../helpers.rb'
+
+lines = read_input('2_part_one_input.txt')
+
+operations = {
+  'forward' => 0,
+  'up' => 0,
+  'down' => 0
+}
+
+lines.each do |line|
+  direction, steps = line.split(' ')
+  operations[direction] += steps.to_i
+end
+
+puts operations['forward'] * (operations['down'] - operations['up'])
